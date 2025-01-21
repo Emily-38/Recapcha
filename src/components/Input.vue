2b353c1f29b5f420b9de34d05243be105522fdde
Emily-38 <martorana.emily38@gmail.com>
2025-01-21 11:55:25 +0100

    feat: add component input and formulaire in app page

---
+++ src/components/Input.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface Props {
+    type: string
+    placeholder: string
+}
+const props = defineProps<Props>()
+const emits = defineEmits(['typing'])
+
+function onInput(e: Event) {
+    const value = (e.target as HTMLInputElement).value
+
+    emits('typing', value)
+}
+</script>
+
+<template>
+
+    <input class="style" :type="props.type" :placeholder="props.placeholder" @input="onInput" >
+
+
+</template>
+
+<style>
+.style {
+    border-radius: 12px;
+    border: 1px solid black;
+    padding: 20px;
+    width: 300px;
+}
+</style>
