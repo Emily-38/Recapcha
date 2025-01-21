78d6d7b5e6c56a08ecdcd2eb44777682e4fcca02
Elodie <elodie.grienay.simplon@gmail.com>
2025-01-21 11:15:23 +0100

    Component bouton

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+interface Props {
+  title: string;
+  type: string;
+  id: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <button class="btnSubmit" type="{{ type }}" id="{{ id }}">{{ title }}</button>
+</template>
+
+<style scoped>
+.btnSubmit {
+  background: #2e5077;
+  color: white;
+  padding: 10px 100px;
+  margin: 5px;
+  border-radius: 0;
+}
+</style>
