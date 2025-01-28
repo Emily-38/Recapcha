bf6dd27743a550222efd633251bd62583306edf2
ShmPt <cacahouettetbs@gmail.com>
2025-01-28 14:44:29 +0100

    feat: hangman game base functions

---
+++ src/components/hangmanGame/guessableWord.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+let props = defineProps([
+    'guessedLetters',
+    'correctLetters'
+])
+
+</script>
+
+<template>
+    <div  class="word-container">
+        <span v-for="letter in props.correctLetters">
+            {{ props.guessedLetters.includes(letter) ? letter : '_' }}
+        </span>
+    </div>
+</template>
+
+<style scoped>
+.word-container {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    justify-content: center;
+    font-size: 50px;
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "./pages/home.vue";
 import QuestionnaireView from "./pages/Questionnaire.vue";
+import HangmanGame from "./pages/hangmanGame.vue";
 
 const routes = [
   { path: "/", component: HomeView },
   { path: "/questionnaire", component: QuestionnaireView },
-
+  { path:"/pendu", component: HangmanGame},
 ];
 
 export const router = createRouter({
