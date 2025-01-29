2e0acb0ef87c0c955962a038acb1a5acfba8211b
Elodie <elodie.grienay.simplon@gmail.com>
2025-01-29 11:05:38 +0100

    redirection

--- src/controllers/JeuController.ts
+++ src/controllers/JeuController.ts
@@ -2,8 +2,15 @@ import { saveReponse } from "../models/JeuModel";
 
 export function submitReponse(reponses: boolean) {
   saveReponse(reponses);
+
+  if (reponses == true) {
+    // changer ici le chemin pour la page de réponse
+    window.location.href = "/bubble";
+  } else {
+    window.location.href = "/result";
+  }
 }
 
-// export function submitReponse(reponses: boolean[]) {
-//   saveReponse(reponses);
+// export function getReponse(reponses: boolean) {
+//   getReponse();
 // }
