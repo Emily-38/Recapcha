9cd50e740aa61e521503df8c5bbbae29e06ef949
Elodie <elodie.grienay.simplon@gmail.com>
2025-01-29 11:14:05 +0100

    route

--- src/controllers/JeuController.ts
+++ src/controllers/JeuController.ts
@@ -4,10 +4,9 @@ export function submitReponse(reponses: boolean) {
   saveReponse(reponses);
 
   if (reponses == true) {
-    // changer ici le chemin pour la page de réponse
-    window.location.href = "/bubble";
-  } else {
     window.location.href = "/result";
+  } else {
+    window.location.href = "/";
   }
 }
 
